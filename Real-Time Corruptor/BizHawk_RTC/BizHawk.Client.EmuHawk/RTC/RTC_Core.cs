8441e6d3143ce039fa38adc8bddf5d797379202f
Phil Girard <philtre@live.ca>
2017-09-09 19:01:44 -0400

    RTC292d

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
@@ -17,7 +17,7 @@ namespace RTC
 
     public static class RTC_Core
     {
-		public static string RtcVersion = "2.92c";
+		public static string RtcVersion = "2.92d";
 		
         public static Random RND = new Random();
         public static string[] args;
