b6502f9b2344b4a2d58955f7194b2e3e5a491be6
Dan B (Narry) <smellyfeetyouhave@gmail.com>
2018-03-24 12:48:54 -0400

    Merge additional GBHawk fixes + hex editor fix

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GBHawk.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Emulation.Cores/Consoles/Nintendo/GBHawk/GBHawk.cs
@@ -44,7 +44,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 		public byte[] OAM = new byte[0xA0];
 
 		public readonly byte[] _rom;
-		public readonly byte[] _bios;	
+		public readonly byte[] _bios;
 		public readonly byte[] header = new byte[0x50];
 
 		public byte[] cart_RAM;
@@ -94,7 +94,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 			{
 				throw new MissingFirmwareException("Missing Gamboy Bios");
 			}
-				
+
 			_bios = Bios;
 
 			Buffer.BlockCopy(rom, 0x100, header, 0, 0x50);
@@ -183,7 +183,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 
 				return new GPUMemoryAreas(iptr0, iptr1, iptr2, iptr3);
 			}
-		} 
+		}
 
 		public GPUMemoryAreas GetGPU() => _gpuMemory;
 
@@ -221,12 +221,6 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 			in_vblank = true; // we start off in vblank since the LCD is off
 			in_vblank_old = true;
 
-			// Start off with RAM all 0xFF (the game 'X' (proto) expects this)
-			for (int i = 0; i < RAM.Length; i++)
-			{
-				RAM[i] = 0xFF;
-			}
-
 			Register_Reset();
 			timer.Reset();
 			ppu.Reset();
@@ -250,39 +244,39 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 
 			switch (header[0x47])
 			{
-				case 0x0: mapper = new MapperDefault();		mppr = "NROM";							break;
-				case 0x1: mapper = new MapperMBC1();		mppr = "MBC1";							break;
-				case 0x2: mapper = new MapperMBC1();		mppr = "MBC1";							break;
-				case 0x3: mapper = new MapperMBC1();		mppr = "MBC1";		has_bat = true;		break;
-				case 0x5: mapper = new MapperMBC2();		mppr = "MBC2";							break;
-				case 0x6: mapper = new MapperMBC2();		mppr = "MBC2";		has_bat = true;		break;
-				case 0x8: mapper = new MapperDefault();		mppr = "NROM";							break;
-				case 0x9: mapper = new MapperDefault();		mppr = "NROM";		has_bat = true;		break;
-				case 0xB: mapper = new MapperMMM01();		mppr = "MMM01";							break;
-				case 0xC: mapper = new MapperMMM01();		mppr = "MMM01";							break;
-				case 0xD: mapper = new MapperMMM01();		mppr = "MMM01";		has_bat = true;		break;
-				case 0xF: mapper = new MapperMBC3();		mppr = "MBC3";		has_bat = true;		break;
-				case 0x10: mapper = new MapperMBC3();		mppr = "MBC3";		has_bat = true;		break;
-				case 0x11: mapper = new MapperMBC3();		mppr = "MBC3";							break;
-				case 0x12: mapper = new MapperMBC3();		mppr = "MBC3";							break;
-				case 0x13: mapper = new MapperMBC3();		mppr = "MBC3";		has_bat = true;		break;
-				case 0x19: mapper = new MapperMBC5();		mppr = "MBC5";							break;
-				case 0x1A: mapper = new MapperMBC5();		mppr = "MBC5";		has_bat = true;		break;
-				case 0x1B: mapper = new MapperMBC5();		mppr = "MBC5";							break;
-				case 0x1C: mapper = new MapperMBC5();		mppr = "MBC5";							break;
-				case 0x1D: mapper = new MapperMBC5();		mppr = "MBC5";							break;
-				case 0x1E: mapper = new MapperMBC5();		mppr = "MBC5";		has_bat = true;		break;
-				case 0x20: mapper = new MapperMBC6();		mppr = "MBC6";							break;
-				case 0x22: mapper = new MapperMBC7();		mppr = "MBC7";		has_bat = true;		break;
-				case 0xFC: mapper = new MapperCamera();		mppr = "CAM";							break;
-				case 0xFD: mapper = new MapperTAMA5();		mppr = "TAMA5";							break;
-				case 0xFE: mapper = new MapperHuC3();		mppr = "HuC3";							break;
-				case 0xFF: mapper = new MapperHuC1();		mppr = "HuC1";							break;
+				case 0x0: mapper = new MapperDefault(); mppr = "NROM"; break;
+				case 0x1: mapper = new MapperMBC1(); mppr = "MBC1"; break;
+				case 0x2: mapper = new MapperMBC1(); mppr = "MBC1"; break;
+				case 0x3: mapper = new MapperMBC1(); mppr = "MBC1"; has_bat = true; break;
+				case 0x5: mapper = new MapperMBC2(); mppr = "MBC2"; break;
+				case 0x6: mapper = new MapperMBC2(); mppr = "MBC2"; has_bat = true; break;
+				case 0x8: mapper = new MapperDefault(); mppr = "NROM"; break;
+				case 0x9: mapper = new MapperDefault(); mppr = "NROM"; has_bat = true; break;
+				case 0xB: mapper = new MapperMMM01(); mppr = "MMM01"; break;
+				case 0xC: mapper = new MapperMMM01(); mppr = "MMM01"; break;
+				case 0xD: mapper = new MapperMMM01(); mppr = "MMM01"; has_bat = true; break;
+				case 0xF: mapper = new MapperMBC3(); mppr = "MBC3"; has_bat = true; break;
+				case 0x10: mapper = new MapperMBC3(); mppr = "MBC3"; has_bat = true; break;
+				case 0x11: mapper = new MapperMBC3(); mppr = "MBC3"; break;
+				case 0x12: mapper = new MapperMBC3(); mppr = "MBC3"; break;
+				case 0x13: mapper = new MapperMBC3(); mppr = "MBC3"; has_bat = true; break;
+				case 0x19: mapper = new MapperMBC5(); mppr = "MBC5"; break;
+				case 0x1A: mapper = new MapperMBC5(); mppr = "MBC5"; has_bat = true; break;
+				case 0x1B: mapper = new MapperMBC5(); mppr = "MBC5"; break;
+				case 0x1C: mapper = new MapperMBC5(); mppr = "MBC5"; break;
+				case 0x1D: mapper = new MapperMBC5(); mppr = "MBC5"; break;
+				case 0x1E: mapper = new MapperMBC5(); mppr = "MBC5"; has_bat = true; break;
+				case 0x20: mapper = new MapperMBC6(); mppr = "MBC6"; break;
+				case 0x22: mapper = new MapperMBC7(); mppr = "MBC7"; has_bat = true; break;
+				case 0xFC: mapper = new MapperCamera(); mppr = "CAM"; break;
+				case 0xFD: mapper = new MapperTAMA5(); mppr = "TAMA5"; break;
+				case 0xFE: mapper = new MapperHuC3(); mppr = "HuC3"; break;
+				case 0xFF: mapper = new MapperHuC1(); mppr = "HuC1"; break;
 
 				// Bootleg mappers
 				// NOTE: Sachen mapper selection does not account for scrambling, so if another bootleg mapper
 				// identifies itself as 0x31, this will need to be modified
-				case 0x31: mapper = new MapperSachen2();	mppr = "Schn2";							break;
+				case 0x31: mapper = new MapperSachen2(); mppr = "Schn2"; break;
 
 				case 0x4:
 				case 0x7:
@@ -302,9 +296,9 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 			}
 
 			// special case for multi cart mappers
-			if ((_rom.HashMD5(0,_rom.Length) == "97122B9B183AAB4079C8D36A4CE6E9C1") ||
+			if ((_rom.HashMD5(0, _rom.Length) == "97122B9B183AAB4079C8D36A4CE6E9C1") ||
 				(_rom.HashMD5(0, _rom.Length) == "9FB9C42CF52DCFDCFBAD5E61AE1B5777") ||
-				(_rom.HashMD5(0, _rom.Length) == "CF1F58AB72112716D3C615A553B2F481")				
+				(_rom.HashMD5(0, _rom.Length) == "CF1F58AB72112716D3C615A553B2F481")
 				)
 			{
 				Console.WriteLine("Using Multi-Cart Mapper");
@@ -333,6 +327,11 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 				case 5:
 					cart_RAM = new byte[0x10000];
 					break;
+				case 0:
+					Console.WriteLine("Mapper Number indicates Battery Backed RAM but none present.");
+					Console.WriteLine("Disabling Battery Setting.");
+					has_bat = false;
+					break;
 			}
 
 			// Sachen maper not known to have RAM
@@ -360,7 +359,7 @@ namespace BizHawk.Emulation.Cores.Nintendo.GBHawk
 					cart_RAM[i] = 0xFF;
 				}
 			}
-			
+
 
 			// Extra RTC initialization for mbc3
 			if (mppr == "MBC3")
