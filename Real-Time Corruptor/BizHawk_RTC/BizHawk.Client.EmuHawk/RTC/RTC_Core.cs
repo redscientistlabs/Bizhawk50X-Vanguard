2f446855a3a550def4046b716e8ff7222948714b
Dan B (Narry) <smellyfeetyouhave@gmail.com>
2018-06-18 00:32:07 -0400

    320n

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
@@ -18,7 +18,7 @@ namespace RTC
 		public static Random RND = new Random();
 
 		//General RTC Values
-		public static string RtcVersion = "3.20m";
+		public static string RtcVersion = "3.20n";
 
 		//Directories
 		public static string bizhawkDir = Directory.GetCurrentDirectory();
