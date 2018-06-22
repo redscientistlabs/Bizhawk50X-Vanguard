827d2f8d90333a1ff64386adbb309ce699e493b5
Dan B (Narry) <smellyfeetyouhave@gmail.com>
2018-06-22 19:33:36 -0400

    Update GlideN64

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Core.cs
@@ -18,7 +18,7 @@ namespace RTC
 		public static Random RND = new Random();
 
 		//General RTC Values
-		public static string RtcVersion = "3.20p";
+		public static string RtcVersion = "3.20q";
 
 		//Directories
 		public static string bizhawkDir = Directory.GetCurrentDirectory();
