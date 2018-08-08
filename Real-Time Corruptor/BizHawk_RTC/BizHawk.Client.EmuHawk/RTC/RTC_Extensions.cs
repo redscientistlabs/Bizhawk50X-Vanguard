5caadb3b786896b1658321fa4cdafbca8f71054d
Dan B <me@narry.land>
2018-08-07 22:27:53 -0400

    Revert "Added lil generic class for keypair values in combobox"
    This reverts commit 4a6e54386d5b1b46f13ac4612af0cceab189f390.

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Extensions.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/RTC_Extensions.cs
@@ -3232,24 +3232,4 @@ namespace RTC
 				}
 			}
 		}*/
-
-	public class ComboBoxTextValue<String, T>
-	{
-		public String Text { get { return _text; }}
-		private String _text;
-
-		public T Value{ get { return _value; }}
-		private T _value;
-
-		public ComboBoxTextValue(String textInput, T valueInput)
-		{
-			_text = textInput;
-			_value = valueInput;
-		}
-
-		public override string ToString()
-		{
-			return Text.ToString();
-		}
-	}
 }
