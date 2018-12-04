09c850e4b4a00edef830a2464793380098698255
NarryG <me@narry.land>
2018-12-04 18:20:58 -0500

    Merge bat fixes

--- Real-Time Corruptor/BizHawk_RTC/output/FactoryClean.bat
+++ Real-Time Corruptor/BizHawk_RTC/output/FactoryClean.bat
@@ -39,9 +39,27 @@ del RTC/PARAMS/*.* /F /Q
 	
 del WGH/PARAMS/*.* /F /Q
 
-start RTC/StateClean.bat
-
-rem IF YOU UPDATE THIS UPDATE StateClean
+del "Apple II\State*.*" /F /Q
+del "Atari 2600\State*.*" /F /Q
+del "Atari 7800\State*.*" /F /Q
+del "Coleco\State*.*" /F /Q
+del "Game Gear\State*.*" /F /Q
+del "Gameboy\State*.*" /F /Q
+del "GBA\State*.*" /F /Q
+del "Genesis\State*.*" /F /Q
+del "Intellivision\State*.*" /F /Q
+del "Libretro\State*.*" /F /Q
+del "Lynx\State*.*" /F /Q
+del "N64\State*.*" /F /Q
+del "NES\State*.*" /F /Q
+del "PC Engine\State*.*" /F /Q
+del "PSX\State*.*" /F /Q
+del "Saturn\State*.*" /F /Q
+del "SG-1000\State*.*" /F /Q
+del "SMS\State*.*" /F /Q
+del "SNES\State*.*" /F /Q
+del "VB\State*.*" /F /Q
+del "WonderSwan\State*.*" /F /Q
 
 del "Apple II\SaveRAM*.*" /F /Q
 del "Atari 2600\SaveRAM*.*" /F /Q
