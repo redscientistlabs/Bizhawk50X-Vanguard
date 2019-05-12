1a36ec8ed1ced3351660ad2419aa75bcadc839e5
Dan B <me@narry.land>
2019-05-12 14:37:27 -0400

    Add SUPPORTS_MIXED_STOCKPILE

--- Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/VanguardCore.cs
+++ Real-Time Corruptor/BizHawk_RTC/BizHawk.Client.EmuHawk/RTC/VanguardCore.cs
@@ -131,6 +131,7 @@ namespace Vanguard
 			partial[VSPEC.SUPPORTS_KILLSWITCH] = true;
 			partial[VSPEC.SUPPORTS_REALTIME] = true;
 			partial[VSPEC.SUPPORTS_SAVESTATES] = true;
+			partial[VSPEC.SUPPORTS_MIXED_STOCKPILE] = true;
 			partial[VSPEC.CONFIG_PATHS] = new[] {Path.Combine(emuDir, "config.ini")};
 
 			return partial;
